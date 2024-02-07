a084fbe3a42ab60f5e6ecd8d6a531fd08f2aff34
George627 <81629113+George627@users.noreply.github.com>
2024-02-07 13:04:45 -0500

    Update index.js
    Added comments.

--- index.js
+++ index.js
@@ -25,30 +25,39 @@ app.get("/", (req, res) => {
 
 //A Post request that request the username from the user. Then render the homepage.ejs.
 app.post("/signin", (req, res) => {
-    
+
+    //Grabs the username that the user submitted for the Post request.
     name = req.body["username"]
-    
+
+    //Renders the homepage.ejs along with the username the user provided.
     res.render("homepage.ejs", {
         username: name
     });
 });
 
+//A Post request that gets the message from the user. Then render the homepage.ejs.
+//Once received, the message is added to the mhistory array to store user's messages.
 app.post("/post", (req, res) => {
-    
+
+    //Creating the m variable to handle the messages that the user sends.
     let m = req.body["messages"];
 
+    //Push the new message into the mhistory Array.
     mhistory.push(m);
-    
+
+    //Renders the homepage.ejs, along with the user's username and message history.
     res.render("homepage.ejs", {
         username: name,
         messages: mhistory
     });
 })
 
+//Renders homepage.ejs.
 app.get("/homepage", (req, res) => {
     res.render("homepage.ejs");
 }); 
 
+//App listening on Port 3000.
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
