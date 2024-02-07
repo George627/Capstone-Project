98c22e0a936aa81e44fdd99fc8fca509d7060c10
George <drumerboygt.gt@gmail.com>
2024-02-06 21:40:44 -0500

    Add for Project

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+let name = "";
+
+let mhistory = [];
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+}); 
+
+app.post("/signin", (req, res) => {
+    name = req.body["username"]
+    
+    res.render("homepage.ejs", {
+        username: name
+    });
+});
+
+app.post("/post", (req, res) => {
+    
+    let m = req.body["messages"];
+
+    mhistory.push(m);
+    
+    res.render("homepage.ejs", {
+        username: name,
+        messages: mhistory
+    });
+})
+
+app.get("/homepage", (req, res) => {
+    res.render("homepage.ejs");
+}); 
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
+
