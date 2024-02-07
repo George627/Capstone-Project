05233a34563a3264420d0f2ee2ff0a5ff01d6e43
George627 <81629113+George627@users.noreply.github.com>
2024-02-07 10:40:15 -0500

    Update index.js
    Added comments

--- index.js
+++ index.js
@@ -2,23 +2,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+//Using the app variable to use express.
 const app = express();
+
+//Using port 3000 for the project.
 const port = 3000;
 
+//Variables that will be used for the user's username and stores their messages in EJS. 
 let name = "";
-
 let mhistory = [];
 
+//Connects the public folder to use images located there.
 app.use(express.static("public"));
 
+//Middleware.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//A Get request that renders the index.ejs.
 app.get("/", (req, res) => {
     res.render("index.ejs");
 }); 
 
+//A Post request that request the username from the user. Then render the homepage.ejs.
 app.post("/signin", (req, res) => {
+    
     name = req.body["username"]
     
     res.render("homepage.ejs", {
